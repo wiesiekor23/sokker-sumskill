df334dc096f76bf60979610f4d59e8af5133931d
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2026-01-03 19:46:36 +0000

    display sumskill

--- sokkerSumskill.js
+++ sokkerSumskill.js
@@ -1,11 +1,12 @@
 document.querySelectorAll("[data-row-id]").forEach( async el => {
-    const skills = (JSON.stringify(await getPlayerSumskill(el.dataset.rowId)));
+    const sumskill = (JSON.stringify(await getPlayerSumskill(el.dataset.rowId)));
+    displaySumskill(el, sumskill);
 })
 
 async function getPlayerSumskill(id) {
     const res = await fetch("https://sokker.org/api/player/" + id);
     const player = await res.json();
-
+    
     const stamina = player.info.skills.stamina;
     const keeper = player.info.skills.keeper;
     const pace = player.info.skills.pace;
@@ -14,14 +15,21 @@ async function getPlayerSumskill(id) {
     const play = player.info.skills.playmaking;
     const pass = player.info.skills.passing;
     const striker = player.info.skills.striker;
-
+    
     const sumskill = Number(stamina) + Number(keeper) + Number(pace) + Number(def) + Number(tech) + Number(play) + Number(pass) + Number(striker);
-
+    
     return sumskill;
 }
 
+function displaySumskill(row, sumskill) {
+    const sumskillContainer = row.querySelector(".table__cell--stop");
 
+    const sumskillDiv = document.createElement("div");
+    sumskillDiv.textContent = sumskill;
+    sumskillDiv.classList.add("sumSkill");
 
+    sumskillContainer.appendChild(sumskillDiv);
+}
 
 
 
